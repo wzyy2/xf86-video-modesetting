e44c0ac62b1b68dba2ddc848fbc80de75d5fe314
U-SH2332IT\wzyy2 <547884940@qq.com>
2016-05-03 04:00:07 +0800

    mess

--- src/accel/rk_exa_rga.c
+++ src/accel/rk_exa_rga.c
@@ -29,8 +29,147 @@ typedef struct
     PixmapPtr pDst;
 } CopyBox;
 
+typedef struct
+{
+    struct rga_image src_img;
+    struct rga_image mask_img;    
+    struct rga_image dst_img;
+
+    char srcRepeat;
+    char srcRotate;
+    double srcScaleX;
+    double srcScaleY;
+
+    char maskRepeat;
+    char maskRotate;
+    double maskScaleX;
+    double maskScaleY;
+    
+} CompositeBox;
+
 static SoildBox gSoildBox;
 static CopyBox gCopyBox;
+static CompositeBox gCompositeBox;
+
+static Bool
+RGACheckPicture(PicturePtr pPicture, char *rot90, double *scaleX, double *scaleY, char* repeat)
+{
+    struct pixman_transform* t;
+
+// #define EPSILON (pixman_fixed_t) (2)
+
+// #define IS_SAME(a, b) (_g2d_check_within_epsilon (a, b, EPSILON))
+// #define IS_ZERO(a)    (_g2d_check_within_epsilon (a, 0, EPSILON))
+// #define IS_ONE(a)     (_g2d_check_within_epsilon (a, F (1), EPSILON))
+// #define IS_UNIT(a)                          \
+//                                     (_g2d_check_within_epsilon (a, F (1), EPSILON) ||  \
+//                                     _g2d_check_within_epsilon (a, F (-1), EPSILON) || \
+//                                     IS_ZERO (a))
+// #define IS_INT(a)    (IS_ZERO (pixman_fixed_frac (a)))
+
+/*RepeatNormal*/
+
+    if(pPicture == NULL)
+    {
+        return TRUE;
+    }
+
+    // if(pPicture->repeat)
+    // {
+    //     switch(pPicture->repeatType)
+    //     {
+    //     case RepeatNormal:
+    //         *repeat = G2D_REPEAT_MODE_REPEAT;
+    //         break;
+    //     case RepeatPad:
+    //         *repeat = G2D_REPEAT_MODE_PAD;
+    //         break;
+    //     case RepeatReflect:
+    //         *repeat = G2D_REPEAT_MODE_REFLECT;
+    //         break;
+    //     default:
+    //         *repeat = G2D_REPEAT_MODE_NONE;
+    //         break;
+    //     }
+    // }
+    // else
+    // {
+    //     *repeat = G2D_REPEAT_MODE_NONE;
+    // }
+
+    if(pPicture->transform == NULL)
+    {
+        *rot90 = 0;
+        *scaleX = 1.0;
+        *scaleY = 1.0;
+        return TRUE;
+    }
+
+    t= pPicture->transform;
+
+    if(!IS_ZERO(t->matrix[0][0]) && IS_ZERO(t->matrix[0][1]) && IS_ZERO(t->matrix[1][0]) && !IS_ZERO(t->matrix[1][1]))
+    {
+        *rot90 = FALSE;
+        *scaleX = pixman_fixed_to_double(t->matrix[0][0]);
+        *scaleY = pixman_fixed_to_double(t->matrix[1][1]);
+    }
+    else if(IS_ZERO(t->matrix[0][0]) && !IS_ZERO(t->matrix[0][1]) && !IS_ZERO(t->matrix[1][0]) && IS_ZERO(t->matrix[1][1]))
+    {
+        /* FIMG2D 90 => PIXMAN 270 */
+        *rot90 = TRUE;
+        *scaleX = pixman_fixed_to_double(t->matrix[0][1]);
+        *scaleY = pixman_fixed_to_double(t->matrix[1][0]*-1);
+    }
+    else
+    {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+static Bool
+RGAIsSupport(PixmapPtr pPix, int Mask)
+{
+    RKPixmapPrivPtr *priv;
+
+    if(!pPix) return TRUE;
+
+    if(Mask != -1 && pPix->drawable.depth < 8)
+        return FALSE;
+
+    priv = exaGetPixmapDriverPrivate(pPixmap);
+    if(!privPixmap->bo)
+        return FALSE;
+
+    if (pPix->drawable.width < 34 || pPix->drawable.height < 34)
+        return FALSE;
+
+    if (pPix->drawable.depth != 32 && pPix->drawable.depth != 24)
+        return FALSE;
+
+    return TRUE;
+}
+
+static int
+RGAGetMode(PixmapPtr pPix)
+{
+    int mode = DRM_FORMAT_XRGB8888;
+
+    switch (pPix->drawable.depth) {
+    case 32:
+        mode = DRM_FORMAT_ARGB8888;
+        break;
+    case 24:
+        if (pPix->drawable.bitsPerPixel == 32)
+            mode = DRM_FORMAT_XRGB8888;
+        else
+            mode = DRM_FORMAT_RGB888;
+        break;
+    }
+
+    return mode;
+}
 
 static Bool
 RKExaRGAPrepareSolid(PixmapPtr pPixmap, int alu, Pixel planemask, Pixel fill_colour)
@@ -40,23 +179,14 @@ RKExaRGAPrepareSolid(PixmapPtr pPixmap, int alu, Pixel planemask, Pixel fill_col
     modesettingPtr ms = modesettingPTR(pScrn);
     int mode, src_fd;
 
-    if (! !priv->bo)
+    /* Check capability */
+    if (!RGAIsSupport(pPixmap, planemask))
+    {
         return FALSE;
+    }
 
     drmPrimeHandleToFD(ms->dev->fd, priv->bo->handle, 0, &src_fd);
 
-    switch (pPixmap->drawable.depth) {
-    case 32:
-        mode = DRM_FORMAT_ARGB8888;
-        break;
-    case 24:
-        if (pPixmap->drawable.bitsPerPixel == 32)
-            mode = DRM_FORMAT_XRGB8888;
-        else
-            mode = DRM_FORMAT_RGB888;
-
-        break;
-    }
 
     fill_colour =
             (fill_colour & 0xff00ff00) + ((fill_colour & 0xff) << 16) +
@@ -67,7 +197,7 @@ RKExaRGAPrepareSolid(PixmapPtr pPixmap, int alu, Pixel planemask, Pixel fill_col
     gSoildBox.solid_img.height = pPixmap->drawable.height;
     gSoildBox.solid_img.stride = pPixmap->drawable.width * 4;
     gSoildBox.solid_img.buf_type = RGA_IMGBUF_GEM;
-    gSoildBox.solid_img.color_mode = mode;
+    gSoildBox.solid_img.color_mode = RGAGetMode(pPixmap);
     gSoildBox.solid_img.fill_color = fill_colour;
     //    xf86DrvMsg(pScrn->scrnIndex, X_INFO, "> %s ptr %x  alu %d planemask %x fill_color %x depth %d width %d height %d stride %d\n",
     //               __func__,  rockchip_bo_map(priv->bo), alu, planemask, fill_colour, pPixmap->drawable.depth, solid_img.width, solid_img.height, solid_img.stride);
@@ -131,17 +261,16 @@ RKExaRGAPrepareCopy(PixmapPtr pSrc, PixmapPtr pDst, int xdir, int ydir,
     RKPixmapPrivPtr priv_dst = exaGetPixmapDriverPrivate(pDst);
     int mode, src_fd, dst_fd;
 
+    /* Check capability */
+    if (!RGAIsSupport(pSrc, planemask) ||
+        !RGAIsSupport(pDst, planemask) ||
+        !xdir || !ydir || alu != 0x03)
+    {
+        return FALSE;
+    }
+
     gCopyBox.pSrc = pSrc;
     gCopyBox.pDst = pDst;
-
-    if (pSrc->drawable.depth != 24 && pSrc->drawable.depth != 24)
-        return FALSE;
-    if (pDst->drawable.depth != 24 && pDst->drawable.depth != 24)
-        return FALSE;
-    if (pSrc->drawable.width < 40 || pSrc->drawable.height < 40)
-        return FALSE;
-    if (pDst->drawable.width < 40 || pDst->drawable.height < 40)
-        return FALSE;
 
         xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                "> %s xdir %d ydir %d  srcdepth %d dstdepth %d srcbo %x dstbo %x\n",
@@ -152,50 +281,24 @@ RKExaRGAPrepareCopy(PixmapPtr pSrc, PixmapPtr pDst, int xdir, int ydir,
     drmPrimeHandleToFD(ms->fd, priv_src->bo->handle, 0, &src_fd);
     drmPrimeHandleToFD(ms->fd, priv_dst->bo->handle, 0, &dst_fd);
 
-    switch (pSrc->drawable.depth) {
-    case 32:
-        mode = DRM_FORMAT_ARGB8888;
-        break;
-    case 24:
-        if (pSrc->drawable.bitsPerPixel == 32)
-            mode = DRM_FORMAT_XRGB8888;
-        else
-            mode = DRM_FORMAT_RGB888;
-
-        break;
-    }
-
     gCopyBox.src_img.bo[0] = src_fd;
     gCopyBox.src_img.width = pSrc->drawable.width;
     gCopyBox.src_img.height = pSrc->drawable.height;
     gCopyBox.src_img.stride = pSrc->drawable.width * 4;
     gCopyBox.src_img.buf_type = RGA_IMGBUF_GEM;
-    gCopyBox.src_img.color_mode = mode;
-
-    switch (pDst->drawable.depth) {
-    case 32:
-        mode = DRM_FORMAT_ARGB8888;
-        break;
-    case 24:
-        if (pDst->drawable.bitsPerPixel == 32)
-            mode = DRM_FORMAT_XRGB8888;
-        else
-            mode = DRM_FORMAT_RGB888;
-
-        break;
-    }
+    gCopyBox.src_img.color_mode = RGAGetMode(pSrc);
 
     gCopyBox.dst_img.bo[0] = dst_fd;
     gCopyBox.dst_img.width = pDst->drawable.width;
     gCopyBox.dst_img.height = pDst->drawable.height;
     gCopyBox.dst_img.stride = pDst->drawable.width * 4;
     gCopyBox.dst_img.buf_type = RGA_IMGBUF_GEM;
-    gCopyBox.dst_img.color_mode = mode;
+    gCopyBox.dst_img.color_mode = RGAGetMode(pDst);
 
     return TRUE;
 }
 
-static void RKExaRGACopy(PixmapPtr pDstPixmap, int srcX, int srcY,
+static void RKExaRGACopySW(PixmapPtr pDstPixmap, int srcX, int srcY,
                          int dstX, int dstY, int width, int height)
 {
     ScrnInfoPtr pScrn = xf86ScreenToScrn(pDstPixmap->drawable.pScreen);
@@ -204,7 +307,7 @@ static void RKExaRGACopy(PixmapPtr pDstPixmap, int srcX, int srcY,
     RKPixmapPrivPtr priv2 = exaGetPixmapDriverPrivate(gCopyBox.pSrc);
     RKPixmapPrivPtr priv3 = exaGetPixmapDriverPrivate(gCopyBox.pDst);
 
-    if (width < 50 || height < 50) {
+
         dumb_bo_map(ms->fd, priv3->bo);
         dumb_bo_map(ms->fd, priv2->bo);
         unsigned int *begin = priv3->bo->ptr;
@@ -217,8 +320,20 @@ static void RKExaRGACopy(PixmapPtr pDstPixmap, int srcX, int srcY,
                         (srcY +
                          i) * gCopyBox.pSrc->drawable.width];
             }
-        }
+        }    
+}
 
+static void RKExaRGACopy(PixmapPtr pDstPixmap, int srcX, int srcY,
+                         int dstX, int dstY, int width, int height)
+{
+    ScrnInfoPtr pScrn = xf86ScreenToScrn(pDstPixmap->drawable.pScreen);
+    RKPixmapPrivPtr priv = exaGetPixmapDriverPrivate(pDstPixmap);
+    modesettingPtr ms = modesettingPTR(pScrn);
+    RKPixmapPrivPtr priv2 = exaGetPixmapDriverPrivate(gCopyBox.pSrc);
+    RKPixmapPrivPtr priv3 = exaGetPixmapDriverPrivate(gCopyBox.pDst);
+
+    if (width < 34 || height < 34) {
+        RKExaRGACopySW(pDstPixmap, srcX, srcY, dstX, dstY, width, height);
     } else  {
                 xf86DrvMsg(pScrn->scrnIndex, X_INFO,
                        "> %s srcX %d srcY %d dstX %d dstY %d width %d height %d\n",
@@ -259,47 +374,62 @@ RKExaRGAPrepareComposite(int op, PicturePtr pSrcPicture, PicturePtr pMaskPicture
                          PicturePtr pDstPicture, PixmapPtr pSrc,
                          PixmapPtr pMask, PixmapPtr pDst)
 {
-    //    switch(pSrc->drawable.depth)
-    //        {
-    //        case 32:
-    //            mode = DRM_FORMAT_ARGB8888;
-    //            break;
-    //        case 24:
-    //            if(pSrc->drawable.bitsPerPixel == 32)
-    //                mode = DRM_FORMAT_XRGB8888;
-    //            else
-    //                mode = DRM_FORMAT_RGB888;
+    // ScrnInfoPtr pScrn = xf86ScreenToScrn(pSrc->drawable.pScreen);
+    // modesettingPtr ms = modesettingPTR(pScrn);
+    // RKPixmapPrivPtr priv_src = exaGetPixmapDriverPrivate(pSrc);
+    // RKPixmapPrivPtr priv_mask = exaGetPixmapDriverPrivate(pMask);    
+    // RKPixmapPrivPtr priv_dst = exaGetPixmapDriverPrivate(pDst);
+    // int mode, src_fd, dst_fd;
 
-    //            break;
-    //        }
+    //     xf86DrvMsg(pScrn->scrnIndex, X_INFO,
+    //            "> %s op %d\n",
+    //            __func__, op);
 
-    //    copy_scr_img.bo[0] = src_fd;
-    //    copy_scr_img.width = pSrc->drawable.width;
-    //    copy_scr_img.height = pSrc->drawable.height;
-    //    copy_scr_img.stride =  pSrc->drawable.width * 4;
-    //    copy_scr_img.buf_type = RGA_IMGBUF_GEM;
-    //    copy_scr_img.color_mode = mode;
+    // /* Check capability */
+    // if (!RGAIsSupport(pSrc, planemask)||
+    //         pMask != NULL ||
+    //     !RGAIsSupport(pDst, planemask))
+    // {
+    //     return FALSE;
+    // }
 
-    //    switch(pDst->drawable.depth)
-    //        {
-    //        case 32:
-    //            mode = DRM_FORMAT_ARGB8888;
-    //            break;
-    //        case 24:
-    //            if(pDst->drawable.bitsPerPixel == 32)
-    //                mode = DRM_FORMAT_XRGB8888;
-    //            else
-    //                mode = DRM_FORMAT_RGB888;
 
-    //            break;
-    //        }
+    // if (!RGACheckPicture(pSrcPicture,
+    //                                 &gCompositeBox.srcRotate,
+    //                                 &gCompositeBox.srcScaleX,
+    //                                 &gCompositeBox.srcScaleY,
+    //                                 &gCompositeBox.srcRepeat))
+    // {
+    //     return FALSE;
+    // }
 
-    //    copy_dst_img.bo[0] = dst_fd;
-    //    copy_dst_img.width = pDst->drawable.width;
-    //    copy_dst_img.height = pDst->drawable.height;
-    //    copy_dst_img.stride =  pDst->drawable.width * 4;
-    //    copy_dst_img.buf_type = RGA_IMGBUF_GEM;
-    //    copy_dst_img.color_mode = mode;
+
+    // drmPrimeHandleToFD(ms->fd, priv_src->bo->handle, 0, &src_fd);
+    // // drmPrimeHandleToFD(ms->fd, priv_mask->bo->handle, 0, &mask_fd);    
+    // drmPrimeHandleToFD(ms->fd, priv_dst->bo->handle, 0, &dst_fd);
+
+
+    // gCompositeBox.src_img.bo[0] = src_fd;
+    // gCompositeBox.src_img.width = pSrc->drawable.width;
+    // gCompositeBox.src_img.height = pSrc->drawable.height;
+    // gCompositeBox.src_img.stride = pSrc->drawable.width * 4;
+    // gCompositeBox.src_img.buf_type = RGA_IMGBUF_GEM;
+    // gCompositeBox.src_img.color_mode = RGAGetMode(pSrc);
+
+
+    // // gCompositeBox.mask_img.bo[0] = mask_fd;
+    // // gCompositeBox.mask_img.width = pMask->drawable.width;
+    // // gCompositeBox.mask_img.height = pMask->drawable.height;
+    // // gCompositeBox.mask_img.stride = pMask->drawable.width * 4;
+    // // gCompositeBox.mask_img.buf_type = RGA_IMGBUF_GEM;
+    // // gCompositeBox.mask_img.color_mode = RGAGetMode(pMask);
+
+    // gCompositeBox.dst_img.bo[0] = dst_fd;
+    // gCompositeBox.dst_img.width = pDst->drawable.width;
+    // gCompositeBox.dst_img.height = pDst->drawable.height;
+    // gCompositeBox.dst_img.stride = pDst->drawable.width * 4;
+    // gCompositeBox.dst_img.buf_type = RGA_IMGBUF_GEM;
+    // gCompositeBox.dst_img.color_mode = RGAGetMode(pDst);
 
     return FALSE;
 }
@@ -312,8 +442,8 @@ RKExaRGAComposite(PixmapPtr pDstPixmap, int srcX, int srcY,
     RKPixmapPrivPtr priv = exaGetPixmapDriverPrivate(pDstPixmap);
     modesettingPtr ms = modesettingPTR(pScrn);
 
-    //    rga_multiple_transform(ms->ctx, &compostie_src_img, &compostie_dst_img, srcX, srcY, test1.width, test1.height,
-    //                           dstX, dstY, width, height, 0, 0, 0);
+   // rga_multiple_transform(ms->ctx, &gCompositeBox.src_img, &gCompositeBox.dst_img, srcX, srcY, gCompositeBox.src_img.width, gCompositeBox.src_img.height,
+   //                        dstX, dstY, width, height, gCompositeBox.srcRotate, 0, 0);
 }
 
 /* done composite : sw done composite, not using pvr2d */
